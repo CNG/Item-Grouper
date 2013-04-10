4cc0f9786e2260ccb2d914d24ca3cbce357d214f
Charlie Gorichanaz <charlie@gorichanaz.com>
2013-04-10 11:33:15 -0700

    Major refactoring: new iteration based method to find good arrangement

--- Readme.markdown
+++ Readme.markdown
@@ -5,72 +5,91 @@ First I built two classes to represent items and groups. I haven't documented th
 
 Then I wrote some functional programming to create a number of items and attempt to group them.
 
-The first strategy I did, which is commented out at the bottom of the file, was roughly: Loop through the items, assigning each one to a group, favoring the group with the most members needing to be met, and then if it's equal, favoring the group with the fewest cumulative time of meeting each group member.
+When I first wrote this, I tried a couple different strategies, all of which were unsatisfactory. I have since simplified, restructured and rewritten this, so you'll have to look at the repo history if interested in how I failed. :P
 
-That code was not solving the problem at all, so I tried a new approach: Loop through the items, and have each item loop through all the other items, saving items that need to be exposed to the first item in a list. If the first item has met all of the other items, then go increase the threshold a bit. Do this for all items and add them to the self selected groups.
+This version employs three strategies to arrive at the optimal scenario:
 
-The second approach solves the problem, but in way too many iterations.
+1. Randomly shuffle groups each session
+2. Generate a bunch (groups*items*50) of combinations and keep the best one
+3. "Smart" group assignment (not finished yet)
 
-I think I am going to try to work on the last approach a bit more. I'll try: If the first item realizes it's met all the rest, instead of increasing the threshold for times having met the others, first move on to another item that still needs to meet someone. I am hopeful this modification will finally solve it... we'll see when I get time to try it.
+I'm currently using the second strategy, as it seems to arrive at an optimal arrangement in a reasonable amount of time. 
 
-Finally I came up with a reasonable way to do this, but it still solves the problem in too many iterations, though a lot better than before.
-
-From the source code:
-
-/**
- * METHOD C
- * This method should get very close to the solution:
- * 1. Copy Item array and shuffle on each iteration. (The effect of this might 
- *    be minimal after the next step, especially after a few iterations.)
- * 2. Sort Items by number of Items to which they have been exposed, in
- *    ascending order.
- * 3. Take the first Item and check for exposure to each other Item, 
- *    starting at the end with the least exposed Items. Each time an 
- *    unexposed Item is happened upon, add to an array of groupmates until
- *    the maximum group size has been reached. If last item is examined and
- *    the maximum group size has not been reached, increase the threshold.
- * 4. Add the Items to a Group and remove the Items from the pending array.
- * 5. Repeat until all Items are in Groups.
- * 
- * Ideally I think we would look at all possible max group size combinations
- * of the available items and favor the group with the maximum overall number
- * of potential new exposures to be made. Then take the remaining items and 
- * do the same, until none are left. This would be a greatly more resource
- * intensive operation, and may not yield much better results, especially for
- * smaller item counts.
- */
+Note: By my calculations, there are 192,972,780 possible combinations (not permutations) of 18 items into 4 groups. It would probably therefore be too processing intensive to generate all combinations, and then test various orders of those combinations...
 
 For example, you can go to this URL: http://charliecorp.com/cmb/?i=%22A%20B%20C%20D%20E%20F%20G%20H%22&g=2
 This is 8 items, 2 groups at a time.
-While loop took 0.001863956451416 seconds.
-After 5 iterations, here is the item info:
-Item A (exposures: B3,C3,D3,E1,F1,G1,H3)
-Item B (exposures: A3,C3,D3,H3,E1,F1,G1)
-Item C (exposures: A3,B3,D3,H3,E1,F1,G1)
-Item D (exposures: A3,B3,C3,H3,E1,F1,G1)
-Item E (exposures: F5,G5,H1,A1,B1,C1,D1)
-Item F (exposures: E5,G5,H1,A1,B1,C1,D1)
-Item G (exposures: E5,F5,H1,A1,B1,C1,D1)
-Item H (exposures: E1,F1,G1,B3,C3,D3,A3)
+
+    Created 8 Item objects
+
+    Starting 800 tests
+
+    Starting 800 tests
+    New high on test 40: 1
+
+    Scenario:
+    Arrangement: (A, B, C, D), (E, F, G, H),
+    Arrangement: (B, D, E, G), (A, F, C, H),
+    Arrangement: (G, C, E, A), (D, F, B, H)
+    Script took 0.32181000709534 seconds to work through 3 arrangements.
 
 For our 18,4 example: http://charliecorp.com/cmb/?i=%22A%20B%20C%20D%20E%20F%20G%20H%20I%20J%20K%20L%20M%20N%20O%20P%20Q%20R%22&g=4
-While loop took 0.011133909225464 seconds.
-After 11 iterations, here is the item info:
-Item A (exposures: B4,C5,D3,E3,L2,O3,H2,I2,J1,R1,K2,N2,F1,M1,Q3,G1,P1)
-Item B (exposures: A4,C4,D4,E3,L4,O4,F1,M2,Q3,G1,K2,H2,J3,P1,R2,I2,N1)
-Item C (exposures: A5,B4,D4,E2,L2,O3,K3,N2,P2,F1,G2,Q3,R2,H2,I1,M1,J1)
-Item D (exposures: A3,B4,C4,E3,L3,O3,H3,I2,J1,M2,Q2,F2,G2,K3,R1,N1,P1)
-Item E (exposures: A3,B3,C2,D3,M2,F2,G1,R3,L3,K2,O2,P2,N2,I1,H2,J2,Q1)
-Item F (exposures: G8,H2,I3,J3,M4,E2,P3,B1,Q3,C1,R1,D2,K1,A1,N3,O2,L1)
-Item G (exposures: F8,H1,I3,J2,M3,E1,Q4,N3,B1,C2,R1,D2,K3,P4,O1,A1,L1)
-Item H (exposures: F2,G1,I5,J7,N2,P1,A2,R3,D3,M1,O2,B2,L2,C2,E2,Q1,K2)
-Item I (exposures: F3,G3,H5,J5,N4,P2,A2,R3,D2,M3,E1,C1,K1,O1,L2,B2,Q1)
-Item J (exposures: F3,G2,H7,I5,N2,P4,A1,R4,D1,M2,O1,Q1,B3,L2,E2,C1,K1)
-Item K (exposures: L2,M2,N2,O3,P4,Q2,R2,E2,C3,A2,B2,D3,F1,G3,I1,H2,J1)
-Item L (exposures: K2,M2,N2,A2,B4,C2,D3,R2,E3,O3,P2,H2,J2,Q3,I2,F1,G1)
-Item M (exposures: K2,L2,N4,E2,F4,G3,Q2,B2,D2,H1,I3,J2,P1,R3,A1,C1,O1)
-Item N (exposures: K2,L2,M4,H2,I4,J2,Q1,G3,C2,P3,E2,A2,F3,O1,R2,D1,B1)
-Item O (exposures: P2,Q4,R2,K3,A3,B4,C3,D3,L3,E2,H2,J1,I1,N1,G1,F2,M1)
-Item P (exposures: O2,Q2,R4,K4,H1,I2,J4,F3,C2,N3,L2,E2,M1,G4,B1,A1,D1)
-Item Q (exposures: O4,P2,R3,K2,G4,M2,N1,B3,F3,C3,D2,J1,L3,A3,H1,I1,E1)
-Item R (exposures: O2,P4,Q3,K2,E3,L2,A1,H3,I3,J4,C2,F1,G1,M3,D1,B2,N2)
+
+    Created 18 Item objects
+    Starting 3600 tests
+    New high on test 44: 0.39869281045752
+    New high on test 470: 0.40522875816993
+
+    Starting 3600 tests
+    New high on test 0: 0.52941176470588
+    New high on test 4: 0.5359477124183
+    New high on test 5: 0.54901960784314
+    New high on test 51: 0.55555555555556
+    New high on test 86: 0.56209150326797
+    New high on test 189: 0.56862745098039
+    New high on test 386: 0.57516339869281
+    New high on test 659: 0.58169934640523
+
+    Starting 3600 tests
+    New high on test 1: 0.67320261437909
+    New high on test 5: 0.70588235294118
+    New high on test 145: 0.72549019607843
+    New high on test 248: 0.73202614379085
+    New high on test 738: 0.73856209150327
+
+    Starting 3600 tests
+    New high on test 0: 0.80392156862745
+    New high on test 2: 0.81045751633987
+    New high on test 22: 0.83006535947712
+    New high on test 173: 0.84313725490196
+    New high on test 556: 0.86928104575163
+
+    Starting 3600 tests
+    New high on test 0: 0.90849673202614
+    New high on test 20: 0.91503267973856
+    New high on test 25: 0.92156862745098
+    New high on test 129: 0.9281045751634
+    New high on test 455: 0.93464052287582
+
+    Starting 3600 tests
+    New high on test 0: 0.96732026143791
+    New high on test 154: 0.98039215686275
+
+    Starting 3600 tests
+    New high on test 2: 0.98692810457516
+    New high on test 7: 0.99346405228758
+    New high on test 15: 1
+
+    Scenario:
+    Arrangement: (A, B, C, D, E), (F, G, H, I, J), (K, L, M, N), (O, P, Q, R),
+    Arrangement: (H, E, K, G, Q), (N, F, C, P, A), (J, L, B, R), (O, M, D, I),
+    Arrangement: (H, N, O, B, I), (G, A, L, E, P), (J, Q, M, C), (D, F, R, K),
+    Arrangement: (F, L, N, E, Q), (O, C, I, K, A), (R, B, G, M), (P, H, J, D),
+    Arrangement: (F, O, E, K, J), (G, D, N, L, C), (P, I, Q, B), (H, R, M, A),
+    Arrangement: (D, O, G, Q, A), (C, I, H, R, N), (E, K, L, B), (J, M, P, F),
+    Arrangement: (Q, A, C, B, F), (L, N, I, J, H), (E, R, G, M), (O, K, P, D),
+    Arrangement: (D, G, I, E, C), (J, F, N, H, A), (Q, B, L, O), (K, R, P, M)
+
+    Script took 12.172544002533 seconds to work through 8 arrangements.
+
+When I get some more time, I'm going to work on a "smart" assignment where items will be grouped into the group with the most items it hasn't been exposed to yet. This is similar to an earlier strategy I tried, so I'm not sure if it will work better.
--- classes.php
+++ classes.php
@@ -2,22 +2,14 @@
 
 /**
  * @author Charlie Gorichanaz
- * @copyright 2012-02-27
+ * @copyright 2013-04-10
  */
 
 class Item {
-  private static $qty = 0;
   private $id = null;
   private $exposures = Array();
   function __construct($id) {
     $this->setID($id);
-    self::$qty++;
-  }
-  function __destruct() {
-    self::$qty--;
-  }
-  public function count() {
-    return self::$qty;
   }
   public function __toString() {
     $exposures = '';
@@ -25,7 +17,7 @@ class Item {
       $exposures .= $key."".$value.",";
     }
     $exposures = substr($exposures, 0, -1);
-    return "Item ".$this->id." (exposures: ".$exposures.")";
+    return "Item ".$this->id." (exposures: ".$this->exposureCount().")";
   }
   private function setID($id) {
     $this->id = $id;
@@ -33,12 +25,19 @@ class Item {
   public function getID() {
     return $this->id;
   }
+  public function expose($item) {
+    if( isset($this->exposures[$item->getID()]) ) {
+      $this->exposures[$item->getID()] = $this->exposures[$item->getID()] + 1;
+    } else {
+      $this->exposures[$item->getID()] = 1;
+    }
+  }
   /**
-   * Return number of exposures to Item supplied or number of Items exposed to.
-   * @param Item $item Item against which this Item will be tests for exposure
-   * @return int Number of exposures to supplied Item or number of Items exposed to
-   */
-  public function exposureCount($item = NULL) {
+  * Return number of exposures to Item supplied or number of Items exposed to.
+  * @param Item $item Item against which this Item will be tests for exposure
+  * @return int Number of exposures to supplied Item or number of Items exposed to
+  */
+  public function exposureCount($item = null) {
     if(isset($item)) {
       if(array_key_exists($item->getID(), $this->exposures)) {
         return $this->exposures[$item->getID()];
@@ -49,35 +48,22 @@ class Item {
       return count($this->exposures);
     }
   }
-  public function expose($item) {
-    $this->exposures[$item->getID()] = $this->exposureCount($item) + 1;
-  }
-  public function deexpose($item) {
-    if ($this->exposureCount($item) > 1) {
-      $this->exposures[$item->getID()] = $this->exposureCount($item) - 1;
-    } else if ($this->exposureCount($item) == 1) {
-      unset($this->exposures[$item->getID()]);
-    } else {
-      throw new Exception("Cannot unexpose $this to $item since $this has never been exposed to $item.");
-    }
-  }
 }
 
 class Group {
   private $maxSize = 0;
   private $mems = Array();
-  private static $qty = 0;
   function __construct($maxSize = 0) {
     $this->setMaxSize($maxSize);
-    self::$qty++;
   }
-  function __destruct() {
-    self::$qty--;
+  function __clone() {
+    $clonedMems = Array();
+    foreach($this->mems as $member){
+      $clonedMems[] = clone $member;
+    }
+    $this->mems = $clonedMems;
   }
-  public function count() {
-    return self::$qty;
-  }
-  public function setMaxSize($maxSize) {
+  private function setMaxSize($maxSize) {
     $this->maxSize = $maxSize;
   }
   public function getMaxSize() {
@@ -88,6 +74,9 @@ class Group {
   }
   public function getMembers() {
     return $this->mems;
+  }
+  public function clear() {
+    $this->mems = Array();
   }
   public function isFull() {
     if ($this->getMaxSize() == 0 || $this->getPopulation() < $this->getMaxSize()) {
@@ -114,19 +103,13 @@ class Group {
   public function hasMember($mem) {
     return in_array($mem, $this->mems);
   }
-  private function memberPosition($mem) {
-    return array_search($mem, $this->mems);
-  }
-  public function removeMember($mem) {
-    if ($this->hasMember($mem)) {
-      unset($this->mems[$this->memberPosition($mem)]);
-      foreach($this->mems as $current_mem) {
-        $current_mem->deexpose($mem);
-        $mem->deexpose($current_mem);
-      }
-    } else {
-      throw new Exception("Cannot remove $mem from $this since $mem is not a member.");
+  // return total exposures of contained items
+  public function exposureCount() {
+    $tempTotal = 0;
+    foreach($this->mems as $mem){
+      $tempTotal += $mem->exposureCount();
     }
+    return $tempTotal;
   }
   public function __toString() {
     $members = '';
@@ -134,7 +117,194 @@ class Group {
       $members .= $mem.", ";
     }
     $members = substr($members, 0, -2);
-    return "Group of ".$this->getPopulation()." (max ".$this->getMaxSize()."): ".$members;
+    return "Group of ".$this->getPopulation().": $members";
+  }
+}
+
+class Arrangement {
+  private $groups = Array();
+  private function addGroup($group) {
+    $this->groups[] = $group;
+  }
+  function __construct($groups = Array()) {
+    foreach($groups as $group){
+      $this->addGroup($group);
+    }
+  }
+  function __clone() {
+    $clonedGroups = Array();
+    foreach($this->groups as $group){
+      $clonedGroups[] = clone $group;
+    }
+    $this->groups = $clonedGroups;
+  }
+  public function getGroups() {
+    return $this->groups;
+  }
+  public function getPopulation() {
+    $itemCount = 0;
+    foreach($this->groups as $group){
+      $itemCount += $group->getPopulation();
+    }
+    return $itemCount;
+  }
+  public function getItems() {
+    $groups = $this->getGroups();
+    $items = Array();
+    foreach($groups as $group){
+      $innerItems = $group->getMembers();
+      foreach($innerItems as $item){
+        $items[] = $item;
+      }
+    }
+    return $items;
+  }
+  public function getEmptyGroups() {
+    $groups = $this->getGroups();
+    foreach($groups as $group){
+      $group->clear();
+    }
+    return $groups;
+  }
+  public function shuffle() {
+    $items = $this->getItems();
+    $groups = $this->getEmptyGroups();
+    shuffle($items);
+    $groupIndex = 0;
+    $lastGroup = count($groups) - 1;
+    foreach($items as $item){
+      while($groups[$groupIndex]->isFull()) {
+        $groupIndex++;
+        if($groupIndex > $lastGroup){
+          throw new Exception("More items than fit in groups. This shouldn't happen.");
+        }
+      }
+      $groups[$groupIndex]->addMember($item);
+    }
+    return $this;
+  }
+  // return average exposure of contained groups' contained items
+  public function exposure() {
+    $exposureCount = 0;
+    foreach($this->groups as $group){
+//  echo "hey".$group->exposureCount()."<br />";
+      $exposureCount += $group->exposureCount();
+    }
+    return $exposureCount / $this->getPopulation() / ( $this->getPopulation() - 1 );
+  }
+  public function __toString() {
+    $groups = '';
+    foreach($this->groups as $group) {
+      $memberObjects = $group->getMembers();
+      $members = '';
+      foreach($memberObjects as $member){
+        $members .= $member->getID().", ";
+      }
+      $members = substr($members, 0, -2);
+      $members = "($members)";
+      $groups .= $members.", ";
+    }
+    $groups = substr($groups, 0, -2);
+    return "Arrangement: $groups";
+  }
+}
+
+class Scenario {
+  private $arrangements = Array();
+  function __construct($items = null, $numGroups = 0) {
+    if ($items && $numGroups) {
+      /**
+       * NOTE: If remainder is not 0, it is also the number of groups that must be
+       * the maximum size. Otherwise the groups won't be as even as possible.
+       */
+      $groupsAtMax = count($items) % $numGroups;
+      $groupMinSize = ( count($items) - $groupsAtMax ) / $numGroups;
+      $groupMaxSize = $groupMinSize + 1;
+      // create groups with appropriate maximums
+      $groups = Array();
+      for($i = 0; $i < $groupsAtMax; $i++) {
+        $groups[] = new Group($groupMaxSize);
+      }
+      for($i = $groupsAtMax; $i < $numGroups; $i++) {
+        $groups[] = new Group($groupMinSize);
+      }
+      // add items to groups
+      $groupIndex = 0;
+      $lastGroup = count($groups) - 1;
+      foreach($items as $item){
+        while($groups[$groupIndex]->isFull()) {
+          $groupIndex++;
+          if($groupIndex > $lastGroup){
+            throw new Exception("More items than fit in groups. This shouldn't happen.");
+          }
+        }
+        $groups[$groupIndex]->addMember($item);
+      }
+      // create arrangement with groups and save
+      $this->addArrangement(new Arrangement($groups));
+    } else {
+      throw new Exception("Must provide item list and number of groups.");
+    }
+  }
+  private function addArrangement($arrangement) {
+    $this->arrangements[] = $arrangement;
+  }
+  private function getLastArrangement() {
+    return $this->arrangements[ count($this->arrangements) - 1 ];
+  }
+  public function getArrangementCount() {
+    return count($this->arrangements);
+  }
+  // return last average exposure of contained groups' contained items
+  public function exposure() {
+    return $this->getLastArrangement()->exposure();
+  }
+  public function nextArrangement($method = 1) {
+    switch($method){
+      case 1:
+        // assign items to groups smartly
+        $arrangement = clone $this->getLastArrangement();
+        $items = $arrangement->getItems();
+        $groups = $arrangement->getEmptyGroups();
+        /*
+        NOT FINISHED
+        Add items to groups based on the group with most members item hasn't met yet
+        */
+        // create arrangement with groups and save
+        $this->addArrangement(new Arrangement($groups));
+        break;
+      case 2:
+        // assign items to groups randomly, calculating exposure and keeping highest one
+        $arrangement = clone $this->getLastArrangement();
+        $highExposure = $arrangement->shuffle()->exposure();
+        $loopIterations = count($arrangement->getGroups()) * $arrangement->getPopulation() * 50;
+        echo "Starting $loopIterations tests<br />";
+        for($i = 0; $i < $loopIterations; $i++){
+          $tempArrangement = clone $this->getLastArrangement();
+          $tempArrangement->shuffle();
+          if($tempArrangement->exposure() > $highExposure){
+            $arrangement = $tempArrangement;
+            $highExposure = $arrangement->exposure();
+            echo "New high on test $i: $highExposure<br />";
+          }
+        }
+        $this->addArrangement($arrangement);
+        echo "<br />";
+        break;
+      case 3:
+        // assign items to groups randomly
+        $arrangement = clone $this->getLastArrangement();
+        $this->addArrangement($arrangement->shuffle());
+        break;
+    }
+  }
+  public function __toString() {
+    $arrangements = '';
+    foreach($this->arrangements as $arrangement) {
+      $arrangements .= $arrangement.",<br />";
+    }
+    $arrangements = substr($arrangements, 0, -7);
+    return "Scenario:<br />$arrangements";
   }
 }
 
